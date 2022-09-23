d6e23d41faec881917001b86e244ecba0c09e008
MerkoRem <iker_rero_m-tsapp1ta2122@cev.com>
2022-09-23 20:11:04 +0200

    Función 2(Multiplicacion)

--- EjercicioGIT/ViewController.swift
+++ EjercicioGIT/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        multiplication(factorPrimero: 5, factorSegundo: 3)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    func multiplication(factorPrimero: Int, factorSegundo: Int){
+        print("El resultaddo es: ",factorPrimero*factorSegundo)
+    }
 
 }
-
