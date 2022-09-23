dcef6b9e49dc23738297d261b7a5f3b0809a3ec2
Abby Dominguez <68694729+abbydomi@users.noreply.github.com>
2022-09-23 23:37:24 +0200

    fixed merge issues

--- EjercicioGIT/ViewController.swift
+++ EjercicioGIT/ViewController.swift
@@ -13,24 +13,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //A
         let ArrayNum = [1,2,3,5,8,15,31]
-        
-        
         for i in 1...ArrayNum.count-1{
             oddOrEven(x: ArrayNum[i])
         }
-            
-        }
+        
         //B
         multiplication(factorPrimero: 5, factorSegundo: 3)
+            
     }
-
+        
     func oddOrEven(x: Int){
         if x % 2 == 0 {
             print("\(x) es par")
         } else {
             print("\(x) es impar")
         }
-}
+    }
 
     func multiplication(factorPrimero: Int, factorSegundo: Int){
         print("El resultaddo es: ",factorPrimero*factorSegundo)
